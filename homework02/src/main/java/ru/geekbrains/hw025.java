fd9713e35f20f685d76bc1774dd57b527d152013
Vladimir <sts79@mail.ru>
2021-02-28 23:03:35 +0300

    add hw025

---
+++ homework02/src/main/java/ru/geekbrains/hw025.java
@@ -0,0 +1,19 @@
+package ru.geekbrains;
+
+public class hw025 {
+
+    public static void main(String[] args) {
+        int min =0;
+        int[] arr5 ={3,8,6,7,2,5,87,54,92};
+        for (int i = 0;i<arr5.length;i++){
+            if (arr5[i]>arr5[i+1]){
+                min = arr5[i];
+            }
+
+        }
+
+
+
+
+    }
+}
