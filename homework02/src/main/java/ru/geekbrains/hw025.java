80959186e24aaa0a31558fc0b55683407d8bd512
VladimirStebakov <77693973+VladimirStebakov@users.noreply.github.com>
2021-03-01 20:07:19 +0300

    Update hw025.java

--- homework02/src/main/java/ru/geekbrains/hw025.java
+++ homework02/src/main/java/ru/geekbrains/hw025.java
@@ -6,10 +6,12 @@ public class hw025 {
         int min =0;
         int[] arr5 ={3,8,6,7,2,5,87,54,92};
         for (int i = 0;i<arr5.length;i++){
-            if (arr5[i]>arr5[i+1]){
+            min = arr5[0];
+
+            if (min>arr5[i]){
                 min = arr5[i];
             }
-
+system.out.println("минимальное значение" + min)
         }
 
 
