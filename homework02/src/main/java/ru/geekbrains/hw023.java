14d0667b740dc20716444b75d4c64fb64c97431c
Vladimir <stvs79@mail.ru>
2021-03-02 15:00:44 +0300

    добавлен метод про високосный год

--- homework02/src/main/java/ru/geekbrains/hw023.java
+++ homework02/src/main/java/ru/geekbrains/hw023.java
@@ -1,56 +1,56 @@
-package ru.geekbrains;
-
-public class hw023 {
-
-    public static boolean eoN (boolean eoN){
-        if (eoN =true) {
-            System.out.println("есть такое место");
-            } else {
-            System.out.println("нет такого места")
-                    }
-    }
-
-
-
-    public static void main(String[] args) {
-
-        int sum = 0;
-
-
-<<<<<<< HEAD
-        int sum1 = 0;
-
-
-
-=======
->>>>>>> master
-        int[] arr3 = {2, 2, 2, 1, 2, 2, 10, 1};
-        for (int i = 0; i < arr3.length; i++) {
-            sum = arr3[i] + sum;
-            int sum1 = 0;
-            for (int j = i + 1; j < arr3.length; j++) {
-
-                sum1 = arr3[j] + sum1;
-
-            }
-<<<<<<< HEAD
-            if (sum == sum1) {eoN(true)
-
-
-            } else {
-                ;
-            }
-=======
-            if (sum == sum1)
-                System.out.println("есть такое место");
-
-            else
-                System.out.println("нет такого места");
->>>>>>> master
-
-
-        }
-
-
-    }
-}
+//package ru.geekbrains;
+//
+//public class hw023 {
+//
+//    public static boolean eoN (boolean eoN){
+//        if (eoN =true) {
+//            System.out.println("есть такое место");
+//            } else {
+//            System.out.println("нет такого места")
+//                    }
+//    }
+//
+//
+//
+//    public static void main(String[] args) {
+//
+//        int sum = 0;
+//
+//
+//<<<<<<< HEAD
+//        int sum1 = 0;
+//
+//
+//
+//=======
+//>>>>>>> master
+//        int[] arr3 = {2, 2, 2, 1, 2, 2, 10, 1};
+//        for (int i = 0; i < arr3.length; i++) {
+//            sum = arr3[i] + sum;
+//            int sum1 = 0;
+//            for (int j = i + 1; j < arr3.length; j++) {
+//
+//                sum1 = arr3[j] + sum1;
+//
+//            }
+//<<<<<<< HEAD
+//            if (sum == sum1) {eoN(true)
+//
+//
+//            } else {
+//                ;
+//            }
+//=======
+//            if (sum == sum1)
+//                System.out.println("есть такое место");
+//
+//            else
+//                System.out.println("нет такого места");
+//>>>>>>> master
+//
+//
+//        }
+//
+//
+//    }
+//}
--- homework02/src/main/java/ru/geekbrains/hw025.java
+++ homework02/src/main/java/ru/geekbrains/hw025.java
@@ -4,15 +4,17 @@ public class hw025 {
 
     public static void main(String[] args) {
         int min =0;
-        int[] arr5 ={3,8,6,7,2,5,87,54,92};
+        int[] arr5 ={3,2,6,7,2,5,87,54,92};
         for (int i = 0;i<arr5.length;i++){
-            min = arr5[0];
+            min = min+0;
 
             if (min>arr5[i]){
                 min = arr5[i];
             }
-system.out.println("минимальное значение" + min)
+    System.out.println("минимальное значение" + min);
+
         }
+
 
 
 
