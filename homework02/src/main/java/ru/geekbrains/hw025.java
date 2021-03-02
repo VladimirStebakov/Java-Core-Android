2d680e4d826ee73e71e715db82242b5e8fa031ab
Vladimir <sts79@mail.ru>
2021-03-02 22:38:52 +0300

    добавлен max и его индекс

--- homework02/src/main/java/ru/geekbrains/hw025.java
+++ homework02/src/main/java/ru/geekbrains/hw025.java
@@ -3,19 +3,32 @@ package ru.geekbrains;
 public class hw025 {
 
     public static void main(String[] args) {
+        int max = 0;
+        int iMax = 0;
         int iMin = 0;
         int min = 0;
         int[] arr5 = {8, 2, 1, -9, 2, -25, 87, 54, 2};
         for (int i = 0; i < arr5.length; i++) {
 
-            if (i == 0) min = arr5[0];
+            if (i == 0) {
+                min = max = arr5[0];
+
+            }
+            if (max < arr5[i]) {
+                max = arr5[i];
+                iMax = i;
+
+            }
 
             if (min > arr5[i]) {
                 min = arr5[i];
                 iMin = i;
+
             }
 
         }
         System.out.println("минимальное значение " + min + " его индекс " + iMin);
+        System.out.println("максимальное значение " + max + " его индекс " + iMax);
+
     }
 }
