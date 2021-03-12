6f79ae3322a26d70df7b202c1c45403492fa8fd5
Vladimir <stvs79@mail.ru>
2021-03-12 16:08:29 +0300

    update shift

--- homework02/src/main/java/ru/geekbrains/hw026.java
+++ homework02/src/main/java/ru/geekbrains/hw026.java
@@ -5,23 +5,27 @@ public class hw026 {
     public static void main(String[] args) {
 
         int i;
+        int j;
+
         int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8};
+
+        int shift = 3;
 
         int buf;
         buf = arr7[0];
-        for (i = 0; i < arr7.length-1; i++) {
+        for (j = 0; j < (shift + 1); j++) {
+            for (i = 0; i < arr7.length - 1; i++) {
 
 
+                arr7[i] = arr7[i + 1];
 
-            arr7[i] = arr7[i + 1];
 
-
-           System.out.print(arr7[i]);
-    }
+                System.out.print(arr7[i]);
+            }
+        }
         arr7[arr7.length - 1] = buf;
 
-            System.out.println(buf);
-
+        System.out.println(buf);
 
 
     }
