8ac3f9258755545899e337aa6440c433acdbf20c
Vladimir <sts79@mail.ru>
2021-03-14 14:31:32 +0300

    сдвиг заработал

--- homework02/src/main/java/ru/geekbrains/hw026.java
+++ homework02/src/main/java/ru/geekbrains/hw026.java
@@ -9,24 +9,29 @@ public class hw026 {
 
         int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        int shift = 3;
+        int shift = 22;
 
         int buf;
-        buf = arr7[0];
-        for (j = 0; j < (shift + 1); j++) {
+
+        for (j = 0; j < (shift); j++) {
+            buf = arr7[0];
             for (i = 0; i < arr7.length - 1; i++) {
 
 
                 arr7[i] = arr7[i + 1];
 
 
-                System.out.print(arr7[i]);
-            }
+               // System.out.print(arr7[i]);
+            }arr7[arr7.length - 1] = buf;
+
+
+
+       // System.out.println(buf);
+    }System.out.println();
+        for (int q=0;q<arr7.length;q++){
+
+            System.out.print(arr7[q]);
         }
-        arr7[arr7.length - 1] = buf;
-
-        System.out.println(buf);
-
 
     }
 }
