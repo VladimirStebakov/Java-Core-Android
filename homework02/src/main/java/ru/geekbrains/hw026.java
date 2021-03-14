1b20105f1c93d31087849808abf071622cb9a6bc
Vladimir <sts79@mail.ru>
2021-03-14 20:55:00 +0300

    сдвиг отлажен

--- homework02/src/main/java/ru/geekbrains/hw026.java
+++ homework02/src/main/java/ru/geekbrains/hw026.java
@@ -4,12 +4,14 @@ public class hw026 {
 
     public static void main(String[] args) {
 
+        long n = 8654178545L;
+
         int i;
         int j;
 
         int[] arr7 = {1, 2, 3, 4, 5, 6, 7, 8};
 
-        int shift = 3;
+        long shift = (arr7.length + (n % arr7.length)) % arr7.length;
 
         int buf;
 
@@ -21,17 +23,20 @@ public class hw026 {
                 arr7[i] = arr7[i + 1];
 
 
-               // System.out.print(arr7[i]);
-            }arr7[arr7.length - 1] = buf;
+                // System.out.print(arr7[i]);
+            }
+            arr7[arr7.length - 1] = buf;
 
 
-
-       // System.out.println(buf);
-    }System.out.println();
+            // System.out.println(buf);
+        }
+        System.out.println();
         for (int k : arr7) {
 
             System.out.print(k);
         }
+        System.out.println();
+        System.out.println("Реальное смещение " + "(" + shift + ")");
 
     }
 }
