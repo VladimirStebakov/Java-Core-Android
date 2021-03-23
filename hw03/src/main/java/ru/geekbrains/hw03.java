e3a261063883266ccbcce5f8f8df1a0dd4e3ff29
Vladimir <stvs79@mail.ru>
2021-03-23 13:58:17 +0300

    запустил задание по кругу

--- hw03/src/main/java/ru/geekbrains/hw03.java
+++ hw03/src/main/java/ru/geekbrains/hw03.java
@@ -7,36 +7,42 @@ import java.util.Scanner;
 public class hw03 {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Хотите отгадать число?");
-        System.out.println("1 - Да  0 - Нет");
-        int pusk = scanner.nextInt();
-        if (pusk == 1) {
+            System.out.println("Хотите отгадать число?");
+            System.out.println("1 - Да  0 - Нет");
+            int pusk = scanner.nextInt();
+            if (pusk == 1) {
 
-            Random rand = new Random();
+                Random rand = new Random();
 
-            int chel = scanner.nextInt();
-            int comp = rand.nextInt(10);
-            //System.out.println(comp);
-            for (int i = 0; i < 3; i++) {
-                System.out.println("Введите число от 0 до 10");
-                chel = scanner.nextInt();
+                int chel = scanner.nextInt();
+                int comp = rand.nextInt(10);
+                //System.out.println(comp);
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Введите число от 0 до 10");
+                    chel = scanner.nextInt();
 
-                if (chel == comp) {
-                    System.out.println("Вы угадали число. Поздравляю!");
-                    break;
+                    if (chel == comp) {
+                        System.out.println("Вы угадали число. Поздравляю!");
+                        System.out.println("============================================");
+                        //break;
+                    }
+                    if (chel > comp)
+                        System.out.println("Ваше число больше загаданного");
+                    if (chel < comp)
+                        System.out.println("Ваше число меньше загаданного");
+
                 }
-                if (chel > comp)
-                    System.out.println("Ваше число больше загаданного");
-                if (chel < comp)
-                    System.out.println("Ваше число меньше загаданного");
+                if (chel != comp) System.out.println("Не угадали(");
+                //break;
+                //System.out.println("Повторить?");
+            } else System.out.println("Как хотите...");
 
-            }
-            if (chel != comp) System.out.println("Не угадали(");
-            //System.out.println("Повторить?");
-        }else System.out.println("Как хотите...");
-
+            System.out.println("=================================================");
+            //return;
+        }
 
     }
 }
